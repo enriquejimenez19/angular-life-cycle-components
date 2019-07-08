b1fe970bb40d9b4f614f6449252dfdff0a6de40d
Osvani Colina <osvacolijim@gmail.com>
2019-07-08 11:50:16 -0400

    Add daughter routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent},
-    { path: 'usuario', component: UsuarioComponent},
+    { 
+        path: 'usuario/:id', 
+        component: UsuarioComponent,
+        children: [
+            { path: 'nuevo', component: UsuarioNuevoComponent},
+            { path: 'detalles', component: UsuarioDetalleComponent},
+            { path: 'editar', component: UsuarioEditarComponent},
+            { path: '**', pathMatch: 'full', redirectTo:'nuevo'}
+        ]
+    },
     { path: '**', pathMatch: 'full', redirectTo:'home'}
 
 ]
